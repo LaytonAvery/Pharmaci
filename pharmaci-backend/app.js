f4a9124e94f52003554f8c40a745e8613f1b5304
LaytonAvery <laytonavery5@gmail.com>
2021-05-19 14:52:09 -0400

    updated port

--- pharmaci-backend/app.js
+++ pharmaci-backend/app.js
@@ -2,7 +2,7 @@ var mysql = require("mysql2");
 var express = require("express");
 var app = express();
 var cors = require("cors");
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 8080;
 app.use(cors());
 var connection = mysql.createConnection({
 	host: "localhost",
